2279f68cae7bfe15c0d9f49fc9ee98455c029f2d
Judy Mwai <juddiemukami@gmail.com>
2022-10-03 12:31:55 +0300

    done

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,4 +1,5 @@
 class BirdsController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :bird_not_found
 
   # GET /birds
   def index
@@ -8,51 +9,39 @@ class BirdsController < ApplicationController
 
   # POST /birds
   def create
-    bird = Bird.create(bird_params)
+    bird = find_bird
     render json: bird, status: :created
   end
 
   # GET /birds/:id
   def show
-    bird = Bird.find_by(id: params[:id])
-    if bird
-      render json: bird
-    else
-      render json: { error: "Bird not found" }, status: :not_found
-    end
+    bird = find_bird
+    render json: bird
+    
   end
 
   # PATCH /birds/:id
   def update
-    bird = Bird.find_by(id: params[:id])
-    if bird
-      bird.update(bird_params)
-      render json: bird
-    else
-      render json: { error: "Bird not found" }, status: :not_found
-    end
+    bird = find_bird
+    bird.update(bird_params)
+    render json: bird
+    
   end
 
   # PATCH /birds/:id/like
   def increment_likes
-    bird = Bird.find_by(id: params[:id])
-    if bird
-      bird.update(likes: bird.likes + 1)
-      render json: bird
-    else
-      render json: { error: "Bird not found" }, status: :not_found
-    end
+    bird = find_bird
+    bird.update(likes: bird.likes + 1)
+    render json: bird
+   
   end
 
   # DELETE /birds/:id
   def destroy
-    bird = Bird.find_by(id: params[:id])
-    if bird
-      bird.destroy
-      head :no_content
-    else
-      render json: { error: "Bird not found" }, status: :not_found
-    end
+    bird = find_bird
+    bird.destroy
+    head :no_content
+    
   end
 
   private
@@ -60,5 +49,14 @@ class BirdsController < ApplicationController
   def bird_params
     params.permit(:name, :species, :likes)
   end
+  def find_bird 
+    Bird.find(params[:id])
+
+  end
+
+  def bird_not_found
+    render json: { error: "Bird not found" }, status: :not_found
+
+  end 
 
 end
